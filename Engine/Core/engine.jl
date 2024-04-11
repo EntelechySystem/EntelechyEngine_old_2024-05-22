f1070d9b3501e85d2e2896fcbcfca526a52f5d0a
Ethan Lin <ElementsPhoenix@outlook.com>
2024-04-12 00:57:07 +0800

    add 引擎、模型初始化

---
+++ Engine/Core/engine.jl
@@ -0,0 +1,16 @@
+"""
+引擎
+"""
+
+# function engine(ParticalAgent, df_agents::Dict, settings_data::Dict, globals::Dict)
+
+## 包含环境
+using Pkg
+Pkg.activate(".")
+
+end  # if
+
+
+
+
+
---
+++ Engine/Core/model.jl
@@ -0,0 +1,12 @@
+
+
+#%% 初始化模型
+
+"""
+初始化个体模型。
+"""
+function initialize_model(; df_agents=df_agents, global_variables=globals)
+
+end
+
+
